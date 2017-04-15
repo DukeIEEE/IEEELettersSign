aa4c82b11c9ac378786758283772ff0e2adfe315
Dhruv K Patel <dkp13@duke.edu>
2017-04-15 15:30:46 -0400

    added initial ledstrip interface

---
+++ code/cmake-build-debug/CMakeFiles/LEDSign.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LEDSign.dir/led_strip.cpp.o"
+  "libLEDSign.pdb"
+  "libLEDSign.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LEDSign.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/led_strip.h
@@ -0,0 +1,26 @@
+/*
+ * Dhruv K Patel
+ */
+
+#ifndef LEDSIGN_LED_STRIP_H
+#define LEDSIGN_LED_STRIP_H
+
+#include <iostream>
+
+using namespace std;
+
+class led_strip {
+
+public:
+
+    led_strip();
+
+    void add_row(int row, int *indicies);
+
+    void set_row_state(int row, int state);
+
+    void set_row_color(int row, int r, int g, int b);
+
+};
+
+#endif //LEDSIGN_LED_STRIP_H
