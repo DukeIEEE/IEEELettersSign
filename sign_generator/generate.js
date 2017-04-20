4051a7e8cc92a915f02f6a17aa561e13379ceccb
Harvey Shi <harveyyshi@gmail.com>
2017-04-20 19:12:54 -0400

    generate.hs

--- sign_generator/generate.js
+++ sign_generator/generate.js
@@ -2,23 +2,26 @@
 // unless otherwise specified, units are inches
 const inch = 72, // 72 dpi
   tabSize = 0.8, // tab width
-  tabMargin = 1.2, // minimum space to leave between tabs
+  tabMargin = 0.8, // minimum space to leave between tabs
   tolerance = 0.01, // padding;
-  th_top = 0.2 + tolerance,
+  th_top = 0.095 + tolerance,
   th_side = 0.2 + tolerance,
   th_bot = th_side + tolerance, // same material for side and bottom
   side_depth = 0.42 + th_top + th_bot,
   side_marg = 0.3, // margin between side panels
-  maxX = 40 * inch,
-  maxY = 28 * inch,
-  w = 9, // width of letter
-  h = 12,
+  maxX = 24 * inch,
+  maxY = 24 * inch,
+  w = 7, // width of letter
+  h = 9.5,
   maxR = 20; // square radius for letter
 
-var draw = SVG('sign').size(maxX, maxY);
+var draw = SVG('sign').size(maxX, maxY)
+total = 0;
 
 drawLetter('I', 0.5, 0.5);
 drawLetter('E', 10.5, 0.5);
+
+console.log(total/12)
 
 document.getElementById('download').addEventListener("click", function() {
   initialize(); // run svg-crowbar
@@ -32,19 +35,21 @@ function drawLetter(letter, x, y) {
   var delta = [];
   if (letter == 'I') {
     arr = drawI(w, h);
-    delta = [w / 2, h / 2];
+    delta = [
+      w / 2,
+      h / 2
+    ];
   } else {
     arr = drawE(w, h);
-    delta = [w, h / 2];
+    delta = [
+      w, h / 2
+    ];
   }
   arr = removeDoubles(arr); // remove doubles from flip operation
   arr = addTabs(arr, x, y); // add tabs
   arr = transform(arr, delta[0], delta[1], inch);
 
-  var polygon = draw.polygon(new SVG.PointArray(arr)).move(xi, yi).fill('none').stroke({
-    opacity: 0.8,
-    width: 0.5
-  })
+  var polygon = draw.polygon(new SVG.PointArray(arr)).move(xi, yi).fill('none').stroke({opacity: 1, width: 0.8})
 }
 
 // the most important fxn
@@ -66,6 +71,7 @@ function addTabs(arr, x, y) {
       p2 = arr[position];
     }
     lineWidth = dist(p1, p2); // in inches
+    total += lineWidth;
 
     // estimate number of tabs to use
     var tabWidth = tabMargin + tabSize,
@@ -73,14 +79,12 @@ function addTabs(arr, x, y) {
       norm = getNormal(arr, p1, p2, i), // find normal vector (outward direction)
       edge = getSign(sub(p2, p1)); // find edge vector
 
-    var leftPad = (i == 0) ? false : -1 == cross(prevNorm, norm) // padding on side
+    var leftPad = (i == 0)
+      ? false
+      : -1 == cross(prevNorm, norm) // padding on side
     prevNorm = norm;
 
-    sidePanels.push({
-      nt: numTabs,
-      wd: lineWidth,
-      lp: leftPad
-    });
+    sidePanels.push({nt: numTabs, wd: lineWidth, lp: leftPad});
     var tabs = getTabPts(numTabs, norm, edge, p1, p2, th_side);
     arr.splice(position, 0, ...tabs); // incorporate new tabs
     i += tabs.length;
@@ -109,21 +113,51 @@ function drawSide(panel, x, y) {
   var nt = panel.nt,
     wd = panel.wd,
     lp = panel.lp,
-    lDelta = lp ? th_side : -th_side, // if leftPad is true, extend one of the sides, if false, move the side in
-    p1 = [-wd / 2, side_depth],
-    p2 = [wd / 2, side_depth],
-    norm = [0, -1], // normal vector is in downward direction
-    edge = [1, 0], // edge vector
-    pts = [p1, [wd / 2 + lDelta, side_depth]];
+    lDelta = lp
+      ? th_side
+      : -th_side, // if leftPad is true, extend one of the sides, if false, move the side in
+    p1 = [-wd / 2,
+      side_depth
+    ],
+    p2 = [
+      wd / 2,
+      side_depth
+    ],
+    norm = [
+      0, -1
+    ], // normal vector is in downward direction
+    edge = [
+      1, 0
+    ], // edge vector
+    pts = [
+      p1,
+      [
+        wd / 2 + lDelta,
+        side_depth
+      ]
+    ];
 
   pts.splice(1, 0, ...getTabPts(nt, norm, edge, p1, p2, th_top));
 
-  var p1 = [wd / 2, 0],
-    p2 = [-wd / 2, 0],
-    norm = [0, 1], // normal vector is in upward direction
-    edge = [-1, 0], // edge vector
+  var p1 = [
+      wd / 2,
+      0
+    ],
+    p2 = [-wd / 2,
+      0
+    ],
+    norm = [
+      0, 1
+    ], // normal vector is in upward direction
+    edge = [
+      -1, 0
+    ], // edge vector
     pts2 = [
-      [wd / 2 + lDelta, 0], p2
+      [
+        wd / 2 + lDelta,
+        0
+      ],
+      p2
     ];
 
   pts2.splice(1, 0, ...getTabPts(nt, norm, edge, p1, p2, th_bot));
@@ -131,15 +165,13 @@ function drawSide(panel, x, y) {
   arr = pts.concat(pts2);
   arr = transform(arr, wd / 2 + x, y, inch);
 
-  var polygon = draw.polygon(new SVG.PointArray(arr)).fill('none').stroke({
-    opacity: 0.8,
-    width: 0.5
-  })
+  var polygon = draw.polygon(new SVG.PointArray(arr)).fill('none').stroke({opacity: 1, width: 0.8})
 }
 
 function getTabPts(numTabs, norm, edge, p1, p2, depth) {
   var numSegments = numTabs + 1,
-    oppEd = [-edge[0], -edge[1]],
+    oppEd = [-edge[0], -edge[1]
+    ],
     delta = div(sub(p2, p1), numSegments), // divide segment into this many segments
     tabs = [],
     sideDelta = mul(norm, depth),
@@ -161,28 +193,51 @@ function getTabPts(numTabs, norm, edge, p1, p2, depth) {
 
 function drawI(w, h) { // generates points on a basic I
   // all units in inches
-  var t1 = 3, // horizontal thickness
-    t2 = 2.5; // vertical thickness
+  var t1 = 2.25, // horizontal thickness
+    t2 = 2; // vertical thickness
   var base = [
-    [0, h / 2],
-    [w / 2, h / 2],
-    [w / 2, h / 2 - t2],
-    [t1 / 2, h / 2 - t2]
+    [
+      0, h / 2
+    ],
+    [
+      w / 2,
+      h / 2
+    ],
+    [
+      w / 2,
+      h / 2 - t2
+    ],
+    [
+      t1 / 2,
+      h / 2 - t2
+    ]
   ];
   return flipY(flipX(base)); // full outline of figure, just need tabs
 }
 
 function drawE(w, h) { // generates points on a basic I
   // all units in inches
-  var t1 = 3, // horizontal thickness
-    t2 = 2.5; // vertical thickness
+  var t1 = 2.25, // horizontal thickness
+    t2 = 2; // vertical thickness
   var base = [
-    [0, h / 2],
-    [w, h / 2],
-    [w, h / 2 - t2],
-    [t1, h / 2 - t2],
-    [t1, t2 / 2],
-    [w, t2 / 2],
+    [
+      0, h / 2
+    ],
+    [
+      w, h / 2
+    ],
+    [
+      w, h / 2 - t2
+    ],
+    [
+      t1, h / 2 - t2
+    ],
+    [
+      t1, t2 / 2
+    ],
+    [
+      w, t2 / 2
+    ]
   ];
   return flipX(base); // full outline of figure, just need tabs
 }
