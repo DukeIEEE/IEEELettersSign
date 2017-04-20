3d834a65954c47ca7d237a4e05e720db8598169f
Simran <simran.singh@duke.edu>
2017-04-20 18:35:38 -0400

    effects

---
+++ IEEELettersSign/code/led_strip.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dhruv K. Patel on 4/14/17.
+//
+
+#include "led_strip.h"
+#include <Arduino.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+    led_strip::led_strip(){
+
+
+
+    };
